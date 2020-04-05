0833b1c1ba72574a422fd478a943f9e851e4bdcb
Rafał Nazarko <rafal.nazarko@gmail.com>
2020-04-05 18:16:14 +0200

    obecnosc fix

--- LSOApp/LSOApp/src/app/app.component.ts
+++ LSOApp/LSOApp/src/app/app.component.ts
@@ -22,8 +22,6 @@ export class AppComponent {
 
 
             onMessageReceivedCallback: (message: firebase.Message) => {
-            //   console.log('[Firebase] onMessageReceivedCallback:');
-            //   console.log(message)
               if(message.foreground)
               {
                 if(this.tabService.opiekun === true)
@@ -31,11 +29,6 @@ export class AppComponent {
                     if(message.body === "Od: Administrator")
                     {
                         this.uiService.showFeedback("succes", "Dostałeś nową wiadomość od Administratora!", 3)
-                        // this.tabService.nowyIndex(2);
-                        // this.uiService.zmienStan(2, true)
-                        // this.wiadosciService.pobierzWiadomosci(1).then(() => {
-                        //     this.uiService.zmienStan(2, false)
-                        // })
                     }
                 }
                 else if(this.tabService.opiekun === false)
@@ -43,21 +36,10 @@ export class AppComponent {
                     if(message.body === "Od: Administrator")
                     {
                         this.uiService.showFeedback("succes", "Dostałeś nową wiadomość od Administratora!", 3)
-                        // this.tabService.nowyIndex(1);
-                        // this.uiService.zmienStan(7,true);
-                        // this.wiadosciService.pobierzWiadomosci(0).then((res) => {
-                        //     this.uiService.zmienStan(7,false);
-                        // });
-
                     }
                     else if(message.body === "Od: Opiekun")
                     {
                         this.uiService.showFeedback("succes", "Dostałeś nową wiadomość od Opiekuna!", 3)
-                        // this.tabService.nowyIndex(1);
-                        // this.uiService.zmienStan(7,true);
-                        // this.wiadosciService.pobierzWiadomosci(0).then((res) => {
-                        //     this.uiService.zmienStan(7,false);
-                        // });
                     }
                     else if(message.title === "Przypomnienie o dyżurze")
                     {
@@ -69,13 +51,11 @@ export class AppComponent {
           })
             .then(() => {
                 firebase.getCurrentPushToken().then((res) => {
-                    // console.log(res)
                     this.http.nadaj_wlasciwosci_urzadzenia(platform.isIOS ? "IOS" : platform.isAndroid ? "Android" : "Inny", res)
                 })
             })
             .catch(error => {
                 firebase.getCurrentPushToken().then((res) => {
-                // console.log(res)
                 this.http.nadaj_wlasciwosci_urzadzenia(platform.isIOS ? "IOS" : platform.isAndroid ? "Android" : "Inny", res)
               })
             });
--- LSOApp/LSOApp/src/app/opiekun/obecnosc/obecnosc.component.ts
+++ LSOApp/LSOApp/src/app/opiekun/obecnosc/obecnosc.component.ts
@@ -86,6 +86,7 @@ export class ObecnoscComponent implements OnInit, OnDestroy {
         this.wydarzeniaService.dzisiejszeWydarzenia(this.aktywnyDzien.getDay())
 
         this.WydarzeniaSub = this.wydarzeniaService.WydarzeniaObecnoscSub.subscribe(lista => {
+            this.ui.zmienStan(0,true)
             this.dzisiejszeWydarzenia = [];
             this.parafiaService.aktualneWydarzenieId = 0
             if(lista !== null)
@@ -154,6 +155,7 @@ export class ObecnoscComponent implements OnInit, OnDestroy {
         });
 
         this.ObecSub = this.parafiaService.Obecnosci.subscribe(lista => {
+
             this.noweObecnosci = [];
 
             if (lista === null) {
@@ -182,11 +184,12 @@ export class ObecnoscComponent implements OnInit, OnDestroy {
                 }
             }
             this.ui.zmienStan(0,false)
-        });
 
+        });
         setTimeout(() => {
             this.opoznienie = true
         }, 3000)
+
     }
 
     ngOnDestroy(): void {
